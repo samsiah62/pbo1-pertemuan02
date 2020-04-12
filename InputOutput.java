9bad43263f1957a41366f03d9534d67695a333fd
samsiah <samsiahnonregbjb@gmail.com>
2020-04-12 11:59:14 +0800

    Compile & Run

--- InputOutput.java
+++ InputOutput.java
@@ -1,4 +1,4 @@
-import java.until.Scanner;
+import java.util.Scanner;
 
 public class InputOutput{
  public static void main(String [] args){
@@ -7,6 +7,5 @@ public class InputOutput{
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
   System.out.println("Hai,  "+nama);
-
  }
 }
